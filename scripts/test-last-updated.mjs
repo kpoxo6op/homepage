c44ee44332f8f8a71af615b6eda2140fa552e3cf
kpoxo6op <kpoxo6op@gmail.com>
2026-07-21 21:55:05 +1200

    Publish Installing the OS

--- scripts/test-last-updated.mjs
+++ scripts/test-last-updated.mjs
@@ -5,6 +5,6 @@ assert.equal(response.status, 200)
 
 const html = await response.text()
 const visibleText = html.replace(/<[^>]+>/g, '')
-assert.match(visibleText, /Last updated May 23, 2025/)
+assert.match(visibleText, /Last updated July 21, 2026/)
 
 console.log('Last-updated marker is visible on a local blog post.')
